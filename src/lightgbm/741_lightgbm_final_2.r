8f763ecb84858f931acfaf938703dd67dcf01cb7
Miguel Angel Sinner <sinnermiguel18@gmail.com>
2022-07-03 11:38:58 -0300

    BO HTa

--- src/lightgbm/741_lightgbm_final_2.r
+++ src/lightgbm/741_lightgbm_final_2.r
@@ -19,7 +19,7 @@ setwd("~/buckets/b1/")   #Establezco el Working Directory
 
 
 kprefijo       <- "KA741"
-ksemilla_azar  <- 102191  #Aqui poner la propia semilla
+ksemilla_azar  <- 442837  #Aqui poner la propia semilla
 kdataset       <- "./datasets/paquete_premium_ext_721.csv.gz"
 
 #donde entrenar
@@ -31,15 +31,15 @@ kfinal_meses_malos  <- c( 202006 )   #meses a excluir del entrenamiento
 #aqui copiar a mano lo menor de la Bayesian Optimization
 # si es de IT y le gusta automatizar todo, no proteste, ya llegara con MLOps
 kmax_bin           <-    31
-klearning_rate     <-     0.0131094708
-knum_iterations    <-   951
-knum_leaves        <-  1002
-kmin_data_in_leaf  <- 16918
-kfeature_fraction  <-     0.5609450635
+klearning_rate     <-     0.0245803513701995
+knum_iterations    <-   513
+knum_leaves        <-  593
+kmin_data_in_leaf  <- 7764
+kfeature_fraction  <-     0.126094601296554
 
 
 
-kexperimento   <- paste0( kprefijo, "0" )
+kexperimento   <- paste0( kprefijo, "2" )
 
 
 
